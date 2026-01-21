e8ab5e44bea8ac792c0a14fc0ab91625a01456c2
rbjonathas <jonathasroberto00@gmail.com>
2026-01-21 20:20:36 +0000

    Correção no .env

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-    import.meta.env.VITE_SUPABASE_URL="https://aiaxiynthbexcwffobkl.supabase.co",
-    import.meta.env.VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpYXhpeW50aGJleGN3ZmZvYmtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NTc0NTQsImV4cCI6MjA4NDQzMzQ1NH0.QY3lhLxUELEomqbGTN4QXulspMHhSqLSEY9IiDdQFkQ"
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
